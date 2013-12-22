0aacc5fd1badb03bfc0561e6aac979dbc2040cbf
Maxlufs <maxlufs@hotmail.com>
2013-12-21 23:43:14 -0800

    INC @io_file.c

---
+++ c/io_file.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+    scanf("format string", variables);
+
+    sprintf(string, "format string", variables);
+    scanf(string, "format string", variables);
+
+    FILE *fp;
+    fp = fopen(name, mode);
+
+    fscanf(fp, "format string", variable list);
+    fprintf(fp, "format string", variable list);
+
+    fclose(fp);
+
+}

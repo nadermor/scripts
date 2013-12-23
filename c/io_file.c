efbdcabb2fd8f150be4f692c2a67b71440aa0ba0
Maxlufs <maxlufs@hotmail.com>
2013-12-23 06:40:49 -0800

    Add char array declaration and definition @io_file.c
    char var[] must be either assigned a number: char var[100], or
    initialized when declared: char var[] = "string"

--- c/io_file.c
+++ c/io_file.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 void main() {
+    // char variables[]; (wrong)
+    // io_file.c: In function ‘main’:
+    // io_file.c:4:10: error: array size missing in ‘variables’
+    
+    // char variables[] = "string"; (correct)
+    // char variables[100]; (correct)
+    
     scanf("format string", variables);
+    printf("%s",variables);
 
-    sprintf(string, "format string", variables);
-    scanf(string, "format string", variables);
+    // sprintf(string, "format string", variables);
+    // scanf(string, "format string", variables);
 
-    FILE *fp;
-    fp = fopen(name, mode);
+    // FILE *fp;
+    // fp = fopen(name, mode);
 
-    fscanf(fp, "format string", variable list);
-    fprintf(fp, "format string", variable list);
+    // fscanf(fp, "format string", variable list);
+    // fprintf(fp, "format string", variable list);
 
-    fclose(fp);
+    // fclose(fp);
 
 }

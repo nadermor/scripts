a1a27daabb57e9bf4cc30719bae89a265a0bacf1
Maxlufs <maxlufs@hotmail.com>
2013-12-27 23:04:43 -0800

    INC @function.c
    Com header library description

---
+++ c/function.c
@@ -0,0 +1,15 @@
+// < stdio.h>  -> defining I/O routines
+// < ctype.h>  -> defining character manipulation routines
+// < string.h> -> defining string manipulation routines
+// < math.h>   -> defining mathematical routines
+// < stdlib.h> -> defining number conversion, storage allocation and similar tasks
+// < stdarg.h> -> defining libraries to handle routines with variable numbers of arguments
+// < time.h>   -> defining time-manipulation routines
+// 
+// In addition, the following header files exist:
+// 
+// < assert.h> -> defining diagnostic routines
+// < setjmp.h> -> defining non-local function calls
+// < signal.h> -> defining signal handlers
+// < limits.h> -> defining constants of the int type
+// < float.h>  -> defining constants of the float type
--- c/io_file.c
+++ c/io_file.c
@@ -6,20 +6,32 @@ void main() {
     // io_file.c:4:10: error: array size missing in ‘variables’
     
     // char variables[] = "string"; (correct)
-    // char variables[100]; (correct)
+    // char variables[20] = "string"; (correct)
+    // char variables[20]; (correct)
     
-    scanf("format string", variables);
-    printf("%s",variables);
+    // scanf("%s", &variables);
+    // printf("%s", variables);
 
     // sprintf(string, "format string", variables);
     // scanf(string, "format string", variables);
 
-    // FILE *fp;
-    // fp = fopen(name, mode);
+    /* I/O to/from files */
+    /* Syntax:
+     * fscanf(fp, "format string", variable list); 
+     * fprintf(fp, "format string", variable list);
+     */
+    int i;
 
-    // fscanf(fp, "format string", variable list);
-    // fprintf(fp, "format string", variable list);
+    /* open a file named foo.dat, 
+     * write Sample Code + 1-10 */
+    FILE *fp; // defined in stdio
+    fp = fopen("foo.dat", "w");
+    // mode: "r" = read, "w" = write, "a" = append
 
-    // fclose(fp);
+    fprintf(fp, "Sample Code\n\n");
+    for (i = 1; i <= 10; i++)
+        fprintf(fp, "i = %d\n", i);
+    fclose(fp);
+    
 
 }

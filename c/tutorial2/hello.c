b773c8a1fc0c46be327245d7ecd627c2def56738
Maxlufs <maxlufs@hotmail.com>
2014-01-06 20:46:53 -0800

    INC @hello.c, @scanf.c

---
+++ c/tutorial2/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World!");
+    // getch();
+    // This command waits for any character input from keyboard.
+    // 'getch()' is not a standard ANSI C function
+    test
+    test
+    test
+    test
+    test
+    test
+    // it's in TurboC lib
+    return 0;
+}
